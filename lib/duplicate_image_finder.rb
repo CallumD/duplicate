f5d8db3d32153660a6f74c744452fd0d14e50759
Callum Dryden <callum.dryden@rightscale.com>
2014-05-24 22:07:15 +0100

    Removing unused dead code from previous version

--- lib/duplicate_image_finder.rb
+++ lib/duplicate_image_finder.rb
@@ -5,10 +5,5 @@ class DuplicateImageFinder
     raise 'I cant work without a file_sorter please provide one' unless args[:file_sorter]
     self.file_sorter = args[:file_sorter]
   end
-
-  def find_duplicates
-    work_dir = file_sorter.working_directory
-    system("findimagedupes -R #{work_dir} > #{work_dir}/dup.txt")
-  end
 end
 
